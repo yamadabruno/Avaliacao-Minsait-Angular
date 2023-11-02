28b5296096ae10c16581839b74cf41e09dc29918
yamadabruno <bruno_yamada@hotmail.com>
2023-11-02 19:19:44 -0300

    ultima alteração

--- AppSpringProduto/src/main/java/br/com/brunoyamada/AppProdutos/model/Produto.java
+++ AppSpringProduto/src/main/java/br/com/brunoyamada/AppProdutos/model/Produto.java
@@ -3,12 +3,18 @@ package br.com.brunoyamada.AppProdutos.model;
 import java.math.BigDecimal;
 import java.util.Objects;
 
+import org.hibernate.validator.constraints.Length;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "produtos")
@@ -19,12 +25,21 @@ public class Produto {
 	private Long id;
 	
 	@Column(nullable = false, unique = true)
+	@NotNull(message = "Campo obrigatório")
+	@NotBlank(message = "Campo não pode ser vazio")
+	@Length(min = 5, max = 20, message = "Campo não pode conter menos que 5 e mais de 20 caracteres")
 	private String codigoBarras;
 	
 	@Column(nullable = false)
+	@NotNull(message = "Nome obrigatório")
+	@NotBlank(message = "Nome não pode ser vazio")
+	@Length(min = 5, max = 100, message = "Nome não pode conter menos que 5 e mais de 100 caracteres")
 	private String nome;
 	
 	@Column(nullable = false)
+	@NotNull(message = "Preço de produto obrigatório")
+	@Max(value = 10, message = "Preço não pode ser maior que 10")
+	@Min(value = 1, message = "Preço não pode ser menor que 1")
 	private BigDecimal preco;
 	
 	public Produto() {}
