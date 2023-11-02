feee9f0e7ad767e53e4e0ad48a7864507691dfa2
yamadabruno <bruno_yamada@hotmail.com>
2023-11-02 03:25:49 -0300

    HTML de Components

--- AppAngularProduto/src/app/app-routing.module.ts
+++ AppAngularProduto/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ProdutosComponent } from './pages/produtos/produtos.component';
+import { CadastrarProdutosComponent } from './pages/cadastrar-produtos/cadastrar-produtos.component';
+import { EditarProdutosComponent } from './pages/editar-produtos/editar-produtos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'produtos', component: ProdutosComponent
+  },
+  {
+    path: 'produtos/cadastrar', component: CadastrarProdutosComponent
+  },
+  {
+    path: 'produtos/editar/:id', component: EditarProdutosComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
