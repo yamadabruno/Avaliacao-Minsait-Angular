28c890a9ed7f5130e5b4013dd9ad09b2c32206ce
yamadabruno <bruno_yamada@hotmail.com>
2023-11-02 04:47:26 -0300

    Page Home

--- AppAngularProduto/src/app/pages/home/home.component.ts
+++ AppAngularProduto/src/app/pages/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Color, ScaleType } from '@swimlane/ngx-charts';
+import { Produto } from 'src/app/model/produto';
+import { ProdutoService } from 'src/app/service/produto.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +10,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  title = 'Produtos Minsait';
+  view: [number, number] = [1000, 400];
+  produtos: Produto[] = [];
 
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Produtos';
+  showYAxisLabel = true;
+  yAxisLabel = 'Preços';
+  timeline = true;
+  legendTitle = 'Legenda';
+
+  colorScheme: Color = {
+    domain: ['#9370DB', '#87CEFA', '#FA8072', '#FF7F50', '#90EE90', '#9370DB'],
+    group: ScaleType.Ordinal, 
+    selectable: true, 
+    name: 'Preço por produto',
+  };
+
+  public single: any = [];
+
+  constructor(private produtoService: ProdutoService) {}
+  ngOnInit() {
+    this.produtoService.buscarTodos().subscribe(
+      {
+        next: produto => {
+          this.single = produto.map(prodMap => ({ name: prodMap.nome, value: prodMap.preco }));
+        }, 
+        error: (error) => {
+          const {message} = error;
+          Swal.fire("Erro de conexão com a aplicação!", message, 'error');
+        }
+      }
+    )
+  }
 }
