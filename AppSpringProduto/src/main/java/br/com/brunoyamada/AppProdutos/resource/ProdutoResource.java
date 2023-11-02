d202c8447448dac0ff95f20d1406e2f096d7f5a7
yamadabruno <bruno_yamada@hotmail.com>
2023-11-02 03:01:34 -0300

    CrossOrigin

--- AppSpringProduto/src/main/java/br/com/brunoyamada/AppProdutos/resource/ProdutoResource.java
+++ AppSpringProduto/src/main/java/br/com/brunoyamada/AppProdutos/resource/ProdutoResource.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import br.com.brunoyamada.AppProdutos.service.ProdutoService;
 
 @RestController
 @RequestMapping("/api/produtos")
+@CrossOrigin(origins = "http://localhost:4200")
 public class ProdutoResource {
 	
 	private ProdutoService produtoService;
