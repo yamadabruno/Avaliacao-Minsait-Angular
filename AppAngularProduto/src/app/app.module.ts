1f0147f27e685f5174286f5e895d90317a914f04
yamadabruno <bruno_yamada@hotmail.com>
2023-11-02 03:17:42 -0300

    Criado component,model,pages e service

--- AppAngularProduto/src/app/app.module.ts
+++ AppAngularProduto/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ProdutosComponent } from './pages/produtos/produtos.component';
+import { CadastrarProdutosComponent } from './pages/cadastrar-produtos/cadastrar-produtos.component';
+import { EditarProdutosComponent } from './pages/editar-produtos/editar-produtos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ProdutosComponent,
+    CadastrarProdutosComponent,
+    EditarProdutosComponent
   ],
   imports: [
     BrowserModule,
---
+++ AppAngularProduto/src/app/pages/editar-produtos/editar-produtos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-editar-produtos',
+  templateUrl: './editar-produtos.component.html',
+  styleUrls: ['./editar-produtos.component.css']
+})
+export class EditarProdutosComponent {
+
+}
